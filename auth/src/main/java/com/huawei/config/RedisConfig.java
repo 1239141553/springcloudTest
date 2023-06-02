0e3e7cfc64b6342749f9988737f9633bc281cd38
winning\l_kx <l_kx@winning.com.cn>
2023-06-02 16:36:51 +0800

    解决包依赖问题

--- auth/src/main/java/com/huawei/config/RedisConfig.java
+++ auth/src/main/java/com/huawei/config/RedisConfig.java
@@ -2,7 +2,7 @@ package com.huawei.config;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
-import org.codehaus.jackson.map.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
